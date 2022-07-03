8014cabf9e4ed3cc46657827ac0e46bbc32252b5
sertachak <sertaconurhakbilen@gmail.com>
2022-07-03 17:25:34 +0300

    initial commit with basic app properties

---
+++ src/main/java/com/sha/jpapagingsortingfiltering/JpaPagingSortingFilteringApplication.java
@@ -0,0 +1,13 @@
+package com.sha.jpapagingsortingfiltering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPagingSortingFilteringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaPagingSortingFilteringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sha/jpapagingsortingfiltering/JpaPagingSortingFilteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha.jpapagingsortingfiltering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPagingSortingFilteringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
